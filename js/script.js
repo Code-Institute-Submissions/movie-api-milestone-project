9484d7e7ce4187080ddaea95fb80e7c9c41334eb
craig ball <craig_5050@msn.com>
2019-04-03 22:11:53 +0000

    added error messages

--- js/script.js
+++ js/script.js
@@ -32,17 +32,35 @@ function movie_search() {
   
   function movie_search() {
         var movie = $( "input" ).val();
-        
+          
+            
   xhr.open("GET", "https://www.omdbapi.com/?t=" + movie +"&plot=full?i=tt3896198&apikey=858bd7c5");
   xhr.send();
  
  xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      movie = (JSON.parse(this.responseText));
+         
+    
+        movie = (JSON.parse(this.responseText));
         document.getElementById("title").textContent = movie.Title;
+        document.getElementById("release").textContent = "Release: " + movie.Released;
+        document.getElementById("director").textContent = "Director: " + movie.Director;
+        document.getElementById("plot").textContent =  movie.Plot;
+        $('#poster').attr('src', 'movie.Poster');
+        
+        
     }
-  };
+    };
+    if (!movie) {
+            document.getElementById("body-header").textContent = "Please Enter A Movie Name";
+            return;
+        }
+        else {
+            document.getElementById("body-header").textContent = "Use The Search Bar Above To Look Up A Movie";
+            return;
+        }
   }
+  
   
 
   /*
@@ -61,4 +79,27 @@ function movie_search() {
   }
 }
 });
-}*/
+}
+ function movie_search() {
+        var movie = $( "input" ).val();
+        
+  xhr.open("GET", "https://www.omdbapi.com/?t=" + movie +"&plot=full?i=tt3896198&apikey=858bd7c5");
+  xhr.send();
+ 
+ xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        if (this.responseText == 'Error.Movie Not Found') {
+            document.getElementById("title").textContent = "Please Try Another Movie Or More Exact Search Terms";
+        }
+    else {
+        movie = (JSON.parse(this.responseText));
+        document.getElementById("title").textContent = movie.Title;
+        document.getElementById("release").textContent = "Release: " + movie.Released;
+        document.getElementById("director").textContent = "Director: " + movie.Director;
+        document.getElementById("plot").textContent =  movie.Plot;
+        $('#poster').attr('src', 'movie.Poster');
+    }
+    }
+  };
+  }
+*/
