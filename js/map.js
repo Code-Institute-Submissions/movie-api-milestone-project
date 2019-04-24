f832112b1581139b97a1bd34684820b9bfedf251
craig ball <craig_5050@msn.com>
2019-04-24 22:05:55 +0000

    cleaned up code

--- js/map.js
+++ js/map.js
@@ -6,91 +6,88 @@ var infowindow;
 
 
 function initMap() {
- 
-  var stevenage = new google.maps.LatLng(51.90224,-0.20256);
-  
-      
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: stevenage,
-          zoom: 13
-        });
 
-       
- 
-$('.map-search-btn').click(function() {
-  
-  service = new google.maps.places.PlacesService(map);
-  
-   var location = document.getElementById("map-search-bar").value;
-   var placeSearch = location.toString();
-    var request = {
-    query: placeSearch,
-    fields: ['name', 'formatted_address', 'geometry', 'opening_hours'],
-  };
-  
-  
-  
-  service.findPlaceFromQuery(request, function(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      var place = results;
-      for (var i = 0; i < place.length; i++) {
-        var lat = location.lat;
-        var lng = location.lng;
-        var locationSearch = new google.maps.LatLng(lat,lng);
-      }
-      map.setCenter(place[0].geometry.location);
-    }
-    findCinema(place[0].geometry.location);
-    console.log(place[0].geometry.location, "debug");
+  var stevenage = new google.maps.LatLng(51.90224, -0.20256);
+
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: stevenage,
+    zoom: 13
   });
-  
-}); 
-    
-    /*----code that uses the latlng to search for nearby cinemas and marks them on the map--------*/
-    function findCinema(locationSearch){
-    var newRequest = {
-          location:  locationSearch,
-          radius: '20000',
-          query: "Cinema",
-          type: ["movie_theater"]
-   };
-   
-   
-   service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(newRequest, callback);
 
-  function callback(results, status) {
-    
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
+
+
+  $('.map-search-btn').click(function() {
+
+    service = new google.maps.places.PlacesService(map);
+
+    var location = document.getElementById("map-search-bar").value;
+    var placeSearch = location.toString();
+    var request = {
+      query: placeSearch,
+      fields: ['name', 'formatted_address', 'geometry', 'opening_hours'],
+    };
+
+
+
+    service.findPlaceFromQuery(request, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        var place = results;
+        for (var i = 0; i < place.length; i++) {
+          var lat = location.lat;
+          var lng = location.lng;
+          var locationSearch = new google.maps.LatLng(lat, lng);
+        }
+        map.setCenter(place[0].geometry.location);
+      }
+      findCinema(place[0].geometry.location);
+    });
+
+  });
+
+  /*----code that uses the latlng to search for nearby cinemas and marks them on the map--------*/
+  function findCinema(locationSearch) {
+    var newRequest = {
+      location: locationSearch,
+      radius: '20000',
+      query: "Cinema",
+      type: ["movie_theater"]
+    };
+
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(newRequest, callback);
+
+    function callback(results, status) {
+
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          var place = results[i];
+          createMarker(results[i]);
+        }
+      }
     }
   }
-}
-    }
-    /*--------code to creATE THE MARKERS---------*/
-   function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var contentString = `<p>${place.name}</p>
-                        <p.>${place.formatted_address}</p>`
-  
-  var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
-        
-  var marker = new google.maps.Marker({
-    map: map,
-    title: place.name,
-    position: place.geometry.location
-  });
-   marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
-    
-   }
+  /*--------code to creATE THE MARKERS---------*/
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var contentString = `<p>${place.name}</p>
+                        `;
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+      map: map,
+      title: place.name,
+      position: place.geometry.location
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+
+  }
 
 
 }
-
-
--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ function movie_search() {
     url: "https://www.omdbapi.com/?s=" + movie + "&type=movie&?&apikey=858bd7c5",
     type: "GET",
     success: function(response) {
-      console.log(response);
       if (response.Response == "False") {
         $('#body-header').innerHtml = "Film Not Found, Please Try Again";
       }
@@ -31,7 +30,7 @@ function movie_search() {
             </div>
           </div>`;
         }
-          $('#movie-container').html(htmlString);
+        $('#movie-container').html(htmlString);
       }
     }
   });
@@ -51,9 +50,6 @@ var xhs = new XMLHttpRequest();
 
 function getMoreInfo(imdbid) {
 
-
-
-
   $.ajax({
     url: "https://www.omdbapi.com/?i=" + imdbid + "&plot=full&?&apikey=858bd7c5",
     type: "GET",
@@ -67,7 +63,6 @@ function getMoreInfo(imdbid) {
   });
 }
 
-
 function createMoreInfoSection(moreInfo) {
   var movieDiv = $(`<div class='result-container'></div>`);
   movieDiv.append($(`<p class="title">Title: ${moreInfo.Title}</p>`));
@@ -78,29 +73,14 @@ function createMoreInfoSection(moreInfo) {
   movieDiv.append($(`<p class="director">Cast: ${moreInfo.Actors}</p>`));
   movieDiv.append($(`<p class="meta">Metascore: ${moreInfo.Metascore}</p>`));
 
-
   return movieDiv;
 }
-
 
 function clearPageAndAddNewResults(resultContainer) {
   $('#movie-container').html("");
 }
 
-// to shorten this: 
-
-// $.get("https://www.omdbapi.com/?i=" + imdbid + "&plot=full&?&apikey=858bd7c5").done(function(response) {
-//   blah blah blah
-// })
-// $.post('someurl.com', {thing1: 'val1', thing2: 'val2'}).done(function(response){
-//   blah blah blah
-// })
-
-
-
 var xhr = new XMLHttpRequest();
-
-
 
 function movie_search2() {
   var movie = $("input").val();
@@ -109,17 +89,16 @@ function movie_search2() {
     url: "https://www.omdbapi.com/?s=" + movie + "&type=movie&?&apikey=858bd7c5",
     type: "GET",
     success: function(response) {
-      var result = response["Search"]; ///puts response into a variable
-      var resultContainer = $("<div class='row'><div class='col-sm-12'></div></div>"); ///specifies where results should go
+      var result = response["Search"];
+      var resultContainer = $("<div class='row'><div class='col-sm-12'></div></div>");
 
-      for (var i = 0; i < result.length; i++) { ///iterate over each result to pick out each piece of data from it
-        movieData = result[i]; /// declaires each seperate result 
-        imdbId = movieData.imdbID; ///gets the id of each film in the result and stores it as a variable for later use
-        movieSection = createMovieSection(movieData); ///assigns the createMovieSection function to this variable
-        resultContainer.append(movieSection); ///get the result of the movieSection variable and then place it into the correct place on the html page
-        console.log(movieData);
+      for (var i = 0; i < result.length; i++) {
+        movieData = result[i];
+        imdbId = movieData.imdbID;
+        movieSection = createMovieSection(movieData);
+        resultContainer.append(movieSection);
       }
-      clearPageAndAddNewResults(resultContainer); ///calls the function to erase the page and add in the results for the new search
+      clearPageAndAddNewResults(resultContainer);
     }
   });
   if (!movie) {
@@ -132,11 +111,11 @@ function movie_search2() {
   }
 }
 
-function createMovieSection(movieData) { ///function to create the section for each movie
-  var movieDiv = $(`<div class='result-container'></div>`); /// a variable that creates the section where the different bits of movie data will sit
-  movieDiv.append($(`<p class="title">Title: ${movieData.Title}</p>`)); ///appends the movie title to the section
+function createMovieSection(movieData) {
+  var movieDiv = $(`<div class='result-container'></div>`);
+  movieDiv.append($(`<p class="title">Title: ${movieData.Title}</p>`));
   movieDiv.append($(`<p class="release"> Release: ${movieData.Year}</p>`))
-  movieDiv.append($(`<img class="poster" src="${movieData.Poster}" alt="poster">`)); ///appends the movie poster
+  movieDiv.append($(`<img class="poster" src="${movieData.Poster}" alt="poster">`));
   movieDiv.append($(`<div><button class="more-info-btn" onclick ="getMoreInfo('${movieData.imdbID}')">More Info</button></div>`));
   return movieDiv;
 }
@@ -149,47 +128,5 @@ function clearPageAndAddNewResults(resultContainer) {
 
 xhr.addEventListener("readystatechange", function() {
   if (this.readyState === this.DONE) {
-    console.log(this.responseText);
   }
 });
-
-
-
-/*
-
-///-------------------this searches by film title and puts all elements into the html but only retrives 1 film----------------------
-var xhr = new XMLHttpRequest();
- 
-
-  function movie_search() {
-        var movie = $( "input" ).val();
-         $(".result-container").show();
-            
-  xhr.open("GET", "https://www.omdbapi.com/?t=" + movie +"?&apikey=858bd7c5");
-  xhr.send();
-  
- xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        movie_data = (JSON.parse(this.responseText));
-         console.log(movie_data);
-   
-        document.getElementById("title").textContent = movie_data.Title;
-        document.getElementById("release").textContent = "Release: " + movie_data.Released;
-        document.getElementById("director").textContent = "Director: " + movie_data.Director;
-        document.getElementById("plot").textContent =  movie_data.Plot;
-        document.getElementById("poster").src =  movie_data.Poster;
-        document.getElementById("cast").textContent = "Cast: " + movie_data.Actors;
-        document.getElementById("metascore").textContent = "Metascore: " + movie_data.Metascore;
-    }
-    
-    };
-    if (!movie) {
-            document.getElementById("body-header").textContent = "Please Enter A Movie Name";
-            return;
-        }
-        else {
-            document.getElementById("body-header").textContent = "Use The Search Bar Above To Look Up A Movie";
-            return;
-        }
-  }
-  */
